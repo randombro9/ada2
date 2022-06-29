5a0e15f150706f3694ac526a9e381f536051f397
randombro9 <101424866+randombro9@users.noreply.github.com>
2022-06-30 00:24:53 +0530

    ss

--- p2.c
+++ p2.c
@@ -1,57 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
- 
-#define BUFF_SIZE 1024
- 
-int main(int argc, char* argv[])
-{
-	int srcFD,destFD,nbread,nbwrite;
-	char *buff[BUFF_SIZE];
-	
-	/*Check if both src & dest files are received*/
-	if(argc != 3)
-	{
-		printf("\nUsage: cpcmd source_file destination_file\n");
-		exit(EXIT_FAILURE);
-	}
- 
-	/*Open source file*/
-	srcFD = open(argv[1],O_RDONLY);
- 
-	if(srcFD == -1)
-	{
-		printf("\nError opening file %s errno = %d\n",argv[1],errno);
-		exit(EXIT_FAILURE);	
-	}
-	
-	/*Open destination file with respective flags & modes
-	  O_CREAT & O_TRUNC is to truncate existing file or create a new file
-	  S_IXXXX are file permissions for the user,groups & others*/
-	destFD = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |    S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
- 
-	if(destFD == -1)
-	{
-		printf("\nError opening file %s errno = %d\n",argv[2],errno);
-		exit(EXIT_FAILURE);
-	}
- 
-	/*Start data transfer from src file to dest file till it reaches EOF*/
-	while((nbread = read(srcFD,buff,BUFF_SIZE)) > 0)
-	{
-		if(write(destFD,buff,nbread) != nbread)
-			printf("\nError in writing data to %s\n",argv[2]);
-	}
-	
-	if(nbread == -1)
-		printf("\nError in reading data from %s\n",argv[1]);
-	
-	if(close(srcFD) == -1)
-		printf("\nError in closing file %s\n",argv[1]);
- 
-	if(close(destFD) == -1)
-		printf("\nError in closing file %s\n",argv[2]);
- 
-	exit(EXIT_SUCCESS);
+
+void merge(int A[],int low,int mid,int high){
+    int i = low;
+    int j = mid+1;
+    int k = low;
+    int B[high+1];
+    while(i<=mid && j<=high){
+        if(A[i] <A[j]){
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else{
+            B[k] = A[j];
+            j++;
+            k++;
+        }
+
+    }
+     while(i<=mid){
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        while(j<=high){
+            B[k] = A[j];
+            j++;
+            k++;
+        }
+       
+    for(int i=low;i<=high;i++){
+        A[i] = B[i];
+    }
+}
+
+void mergeSort(int A[], int low,int high){
+int mid;
+if(high>low){
+     mid=(low+high)/2;
+    mergeSort(A, low,mid);
+    mergeSort(A,mid+1,high);
+    merge(A,low,mid,high);
+}
+}
+
+
+
+
+int main(){
+    
+    int arr[] = {23,556,1,10,2,556,21,234,1000,7};
+    mergeSort(arr,0,9);
+
+    for(int k=0;k<10;k++){
+        printf("%d\n", arr[k]);
+    }
+
+    return 0;
 }
